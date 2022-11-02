1b350e97c72a517f6c94123429f2bf61fe0d1c27
narendra28051991 <narendra28051991@gmail.com>
2022-11-02 10:05:21 +0100

    pokemon-page routes created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import Pokemon from './pages/pokemon/Pokemon';
+import Navbar from './components/navbar/Navbar';
+import Search from './pages/search/Search';
+
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/pokemon/:id' element={<Pokemon />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
